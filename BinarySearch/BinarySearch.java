76d30f07e0375dfced2307c97303d1aa4da458a6
hamzapro305 <hamzapro285@gmail.com>
2022-07-04 20:21:53 +0500

    Binary Search Fix

--- BinarySearch/BinarySearch.java
+++ BinarySearch/BinarySearch.java
@@ -1,21 +1,38 @@
 package BinarySearch;
 
+import java.util.Arrays;
+
 public class BinarySearch {
     public int BS(int[] a, int s){
         int si = 0;
         int ei = a.length - 1;
         int mid = (si + ei)/2;
-        while(a[mid] != s){
-            if(si >= ei) { return 0; }
+        while(true){
+            System.out.println(Arrays.toString(a) + " " + si + " " + ei + " " + " " + mid);
+            if(si > ei) { return 0; }
             else if (s == a[mid]) { return 1; }
             else if(s > a[mid]) { si = mid + 1; }
             else if(s < a[mid]) { ei = mid - 1; }
             mid = (si + ei) / 2;
         }
-        return 0;
+    }
+
+    public static void sort(int a[]){
+        for(int i = 0; i < a.length; i++) {
+           for (int j = 0; j < a.length; j++) {
+
+                if(a[i] < a[j]){
+                    int temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
+
+           } 
+        }
     }
     public static void main(String[] args){
         int MyArray[] = {7, 3, 12, 2, 17, 9, 3, 13, 1};
+        sort(MyArray);
 
         BinarySearch bs = new BinarySearch();
         int SearchedElement = bs.BS(MyArray, 9);
